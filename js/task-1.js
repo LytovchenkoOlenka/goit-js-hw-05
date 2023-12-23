5960bba215f0ad889a1f9302c729db08e95edb79
LytovchenkoOlenka <131744554+LytovchenkoOlenka@users.noreply.github.com>
2023-12-23 19:56:48 +0100

    HW

---
+++ js/task-1.js
@@ -0,0 +1,13 @@
+function slugify(title) {
+  const titleLowerCase = title.toLowerCase();
+  const arraySlug = titleLowerCase.split(' ');
+  const slug = arraySlug.join('-');
+
+  return slug;
+}
+
+console.log('_______________Task - 1_______________');
+console.log(slugify('Arrays for begginers')); // "arrays-for-begginers"
+console.log(slugify('English for developer')); // "english-for-developer"
+console.log(slugify('Ten secrets of JavaScript')); // "ten-secrets-of-javascript"
+console.log(slugify('How to become a JUNIOR developer in TWO WEEKS')); // "how-to-become-a-junior-developer-in-two-weeks"
